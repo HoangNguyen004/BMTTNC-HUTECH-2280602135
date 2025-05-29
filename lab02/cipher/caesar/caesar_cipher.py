642bddfee254d0503e14971a5de985cfb0dd28b0
HoangNguyen004 <hoangnguyenssj004@gmail.com>
2025-05-29 11:20:55 +0700

    buoi3

--- lab02/cipher/caesar/caesar_cipher.py
+++ lab02/cipher/caesar/caesar_cipher.py
@@ -1,28 +1,31 @@
+# cipher/caesar.py
 from string import ascii_uppercase
 
 ALPHABET = ascii_uppercase
 
 class CaesarCipher:
-    def encrypt_text(self, text: str, key: int) -> str:
+    @staticmethod
+    def encrypt_text(text: str, key: int) -> str:
         alphabet_len = len(ALPHABET)
         encrypted_text = ""
-        for letter in text:
+        for letter in text.upper():
             if letter in ALPHABET:
                 letter_index = ALPHABET.index(letter)
                 encrypted_index = (letter_index + key) % alphabet_len
                 encrypted_text += ALPHABET[encrypted_index]
             else:
                 encrypted_text += letter
-        return "".join(encrypted_text)
+        return encrypted_text
 
-    def decrypt_text(self, text: str, key: int) -> str:
+    @staticmethod
+    def decrypt_text(text: str, key: int) -> str:
         alphabet_len = len(ALPHABET)
         decrypted_text = ""
-        for letter in text:
+        for letter in text.upper():
             if letter in ALPHABET:
                 letter_index = ALPHABET.index(letter)
                 decrypted_index = (letter_index - key) % alphabet_len
                 decrypted_text += ALPHABET[decrypted_index]
             else:
                 decrypted_text += letter
-        return "".join(decrypted_text)
+        return decrypted_text
